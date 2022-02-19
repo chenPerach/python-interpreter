febcab3554528d2bc18c8de61d832840240122cf
chen <chen76273@gmail.com>
2022-02-19 15:59:28 +0200

    more tests

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -4,9 +4,22 @@ def test_spaces():
     assert Interpreter('5 +4 ').expr() == 9
     assert Interpreter(' 5+ 4 ').expr() == 9
 
+def test_add():
+    assert Interpreter('5+4').expr() == 9
+    assert Interpreter('5+6').expr() == 11
+    assert Interpreter('2+6').expr() == 8
+    assert Interpreter('0+6').expr() == 6
+    
+def test_sub():
+    assert Interpreter('5-0').expr() == 1
+    assert Interpreter('5-6').expr() == -1
+    assert Interpreter('2-6').expr() == -4
+    assert Interpreter('0-6').expr() == -6
+    
 
 def test_long_numbers():
-    assert Interpreter('55 + 4').expr() == 59
+    assert Interpreter('55 + 53426').expr() == 53481
+    assert Interpreter('53426+55').expr() == 53481
 
 
 def test_arithmatics():
